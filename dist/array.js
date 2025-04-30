5003ff9d9f99ca445bb8c8e64d31d7e01684e8fc
Chan Badsha Bhuiyan <chanbadsha005@gmail.com>
2025-04-30 23:25:05 +0600

    type of object in ts

---
+++ dist/array.js
@@ -0,0 +1,7 @@
+"use strict";
+// there are 2 data types of ts
+// 1. Primitive data type and 2. Non Primitive Data type
+// Primitive --> string,number,boolean, undefined, null,symbol
+// Non Primitive --> Object , array, Tuple
+let age = [50, 60];
+let ageCount = ["Mr Badsha", 21, true];
---
+++ dist/object.js
@@ -0,0 +1,10 @@
+"use strict";
+const workerInfo = {
+    companyName: "Programmig Hero BD",
+    role: "Frontend Dev",
+    firstName: "Chan",
+    middleName: "Badsha",
+    lastName: "Bhuiyan",
+    isPresent: true
+};
+console.log(workerInfo);
